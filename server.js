3b91ef5a3499720eaadde8c65921ad4469607b67
Miguel <miguelcordova111@gmail.com>
2026-03-24 13:12:59 -0700

    fix: habilitar CORS

--- server.js
+++ server.js
@@ -3,6 +3,15 @@ const app         = jsonServer.create()
 const router      = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
 
+// ← Esto es lo que faltaba: habilitar CORS
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', '*')
+  res.header('Access-Control-Allow-Methods', 'GET, POST, DELETE, OPTIONS')
+  if (req.method === 'OPTIONS') return res.sendStatus(200)
+  next()
+})
+
 app.use(middlewares)
 app.use(jsonServer.bodyParser)
 app.use(router)
